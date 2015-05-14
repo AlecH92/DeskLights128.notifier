98f7ab01ab001642c677eaf9f76d43db2b45fff8
Alec Harley <alec.91@gmail.com>
2015-05-14 00:43:39 -0700

    Show version name on Settings page
    Added option to show version name on settings page.

--- app/src/main/java/com/hilltoprobotics/dl128notifier/Settings.java
+++ app/src/main/java/com/hilltoprobotics/dl128notifier/Settings.java
@@ -1,6 +1,9 @@
 package com.hilltoprobotics.dl128notifier;
 
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 
 
@@ -12,5 +15,12 @@ public class Settings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
  
         addPreferencesFromResource(R.xml.settings);
+        EditTextPreference editTextPref = (EditTextPreference) findPreference("appver");
+        try {
+            editTextPref
+                    .setSummary(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        };
     }
 }
