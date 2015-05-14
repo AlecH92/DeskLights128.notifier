a9e932683dac7068a1b3bee3db496cad5d241e3a
Alec Harley <alec.91@gmail.com>
2015-05-14 00:30:00 -0700

    Feature #1 Enabled at top of list
    Now shows Currently Enabled notification applications, and then a list
    of All Applications.

--- app/src/main/java/com/hilltoprobotics/dl128notifier/MainActivity.java
+++ app/src/main/java/com/hilltoprobotics/dl128notifier/MainActivity.java
@@ -13,10 +13,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +29,11 @@ public class MainActivity extends ActionBarActivity {
     List<ApplicationInfo> packages;
     ArrayList<String> packagesInfoName;
     ArrayList<String> packagesRealName;
+    ArrayList<String> packagesNew;
     HashMap<String, String> aList;
+    HashMap<String, String> theMap;
+    FileInputStream fileIn;
+    Integer packageCount = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,38 +43,125 @@ public class MainActivity extends ActionBarActivity {
         thisList = (ListView) findViewById(R.id.listView);
         packagesInfoName = new ArrayList<>();
         packagesRealName = new ArrayList<>();
+        packagesNew = new ArrayList<>();
         aList = new HashMap<>();
+        theMap = new HashMap<>();
+        packageCount = 2;
+
+        String[] pkgList = fileList();
+        for(String thePackage : pkgList) {
+            try {
+                fileIn = openFileInput(thePackage);
+                byte[] buffer = new byte[(int) fileIn.getChannel().size()];
+                fileIn.read(buffer);
+                String str = "";
+                for (byte b : buffer) str += (char) b;
+                fileIn.close();
+
+                Properties props = new Properties();
+                props.load(new StringReader(str.substring(1, str.length() - 1).replace(", ", "\n")));
+                for (Map.Entry<Object, Object> e : props.entrySet()) {
+                    theMap.put((String) e.getKey(), (String) e.getValue());
+                }
+
+                boolean isEnabled = Boolean.parseBoolean(theMap.get("checked"));
+                if(isEnabled) {
+                    Log.d("DL128", "Enabled: " + thePackage);
+                    packageCount++;
+                }
+                else {
+                    Log.d("DL128", "Disabled: " + thePackage);
+                }
+            }
+            catch (IOException e) {
+            }
+        }
+
+
 
         packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
         for (ApplicationInfo packageInfo : packages) {
             try {
                 String theData = (String) pm.getApplicationLabel(pm.getApplicationInfo(packageInfo.packageName, PackageManager.GET_META_DATA));
                 packagesInfoName.add(theData + " - " + packageInfo.processName);
-                //packagesRealName.add(packageInfo.processName);
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
         }
         Collections.sort(packagesInfoName);
-        int i = 0;
-        for (String str : packagesInfoName) {
-            String[] parts = str.split(" - ");
-            aList.put(String.valueOf(i),parts[1]);
-            packagesRealName.add(parts[1]);
-            i++;
+
+        packagesNew.add("Currently Enabled:");
+
+        for(String thePackage : pkgList) {
+            try {
+                fileIn = openFileInput(thePackage);
+                byte[] buffer = new byte[(int) fileIn.getChannel().size()];
+                fileIn.read(buffer);
+                String str = "";
+                for (byte b : buffer) str += (char) b;
+                fileIn.close();
+
+                Properties props = new Properties();
+                props.load(new StringReader(str.substring(1, str.length() - 1).replace(", ", "\n")));
+                for (Map.Entry<Object, Object> e : props.entrySet()) {
+                    theMap.put((String) e.getKey(), (String) e.getValue());
+                }
+
+                boolean isEnabled = Boolean.parseBoolean(theMap.get("checked"));
+                if(isEnabled) {
+                    String theData = (String) pm.getApplicationLabel(pm.getApplicationInfo(thePackage, PackageManager.GET_META_DATA));
+                    packagesNew.add(theData + " - " + pm.getApplicationInfo(thePackage, PackageManager.GET_META_DATA).processName);
+                }
+                else {
+                }
+            }
+            catch (IOException e) {
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,android.R.id.text1,packagesInfoName);
+        packagesNew.add("All apps:");
+        packagesNew.addAll(packagesInfoName);
+        Log.d("DL128", "packageCount: " + packageCount);
+
+        int i = 0;
+        for (String str : packagesNew) {
+            if(i==0) {
+                aList.put(String.valueOf(i),"system");
+                packagesRealName.add("system");
+                i++;
+            }
+            else if(i==packageCount-1) {
+                aList.put(String.valueOf(i),"system");
+                packagesRealName.add("system");
+                i++;
+            }
+            else {
+                String[] parts = str.split(" - ");
+                aList.put(String.valueOf(i), parts[1]);
+                packagesRealName.add(parts[1]);
+                i++;
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,android.R.id.text1,packagesNew);
         thisList.setAdapter(adapter);
         thisList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("DL128","Value of pos " + String.valueOf(position));
-                Intent intent = new Intent(MainActivity.this,SetInformation.class);
+                if(position==0) {
+                    return;
+                }
+                if(position==packageCount-1) {
+                    return;
+                }
+                Log.d("DL128", "Value of pos " + String.valueOf(position));
+                Intent intent = new Intent(MainActivity.this, SetInformation.class);
                 intent.putExtra("pkgName", aList.get(String.valueOf(position)));
-                intent.putExtra("title","error");
+                intent.putExtra("title", "error");
                 try {
-                    intent.putExtra("title",(String) pm.getApplicationLabel(pm.getApplicationInfo(aList.get(String.valueOf(position)), PackageManager.GET_META_DATA)));
+                    intent.putExtra("title", (String) pm.getApplicationLabel(pm.getApplicationInfo(aList.get(String.valueOf(position)), PackageManager.GET_META_DATA)));
                 } catch (PackageManager.NameNotFoundException e) {
                     e.printStackTrace();
                 }
